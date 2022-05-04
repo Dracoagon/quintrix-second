2125cc20eaabd10157e2ac676302680cd7e8ee41
Dracoagon <drakeaus.taylor@gmail.com>
2022-05-04 15:10:11 -0700

    initial setup

---
+++ src/test/java/com/quintrix/jfs/quintrixsecond/QuintrixSecondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quintrix.jfs.quintrixsecond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuintrixSecondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
